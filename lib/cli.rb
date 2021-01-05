d208d68a7ccaadc71f7ca4ab3257cc5678b78978
gjeffgolden <gjgolden08@gmail.com>
2021-01-05 15:35:16 -0700

    Updated cli.rb to include welcome method.

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,10 +6,15 @@ class Cli
         TTY::Prompt.new
     end
 
+    def menu
+        Beverage.all.pluck("name")
+    end
+
     def welcome
         system("clear")
         puts "Cheers! Welcome to Spice Melange. What's your name, traveler?"
+        name = gets.chomp
+        prompt.select("Have a seat, #{name}. Would you like to see our menu, or create your own drink?", menu)
     end
-
 
 end
